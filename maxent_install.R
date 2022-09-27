2d7386ee6c72de161af60b93c0ec698bccdf8fde
Peter Schafran <ps997@cornell.edu>
2022-09-27 16:55:00 -0400

    upload file

---
+++ maxent_install.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+
+install.packages("rgeos")
+install.packages("raster")
+install.packages("sp")
+install.packages("maps")
+install.packages("maxentHelper.R")
+install.packages("mapdata")
+install.packages("rgbif")
+install.packages("rgdal")
+install.packages("sf")
+install.packages("dplyr")
+install.packages("dismo")
+install.packages("rJava")
+install.packages("rinat")
+install.packages("ridigbio")
+install.packages("CoordinateCleaner")
+install.packages("spThin")
+
