37d83ab95e9cec6640bd9cf81387e7c4832855c6
Peter Schafran <ps997@cornell.edu>
2023-07-14 09:35:26 -0400

    fix typo

--- plotCovAlongContigs.py
+++ plotCovAlongContigs.py
@@ -172,7 +172,7 @@ runningyMin = 0
 for key in contigDict.keys():
 	#smoothingBases = int(len(contigDict[key][1]) * (smoothingFactor/100))
 	#movAvgDict[key] = [np.array(running_mean(contigDict[key][0], avgingWindowSize)).tolist(), np.array(running_mean(contigDict[key][1], avgingWindowSize)).tolist()]
-	movAvgDict[key] = [np.array([contigDict[key][0]], np.convolve(contigDict[key][1], avgingWindowSize, mode = "valid").tolist())]
+	movAvgDict[key] = [np.array(contigDict[key][0], [np.convolve(contigDict[key][1], avgingWindowSize, mode = "valid")].tolist())]
 	if yMin == False and runningyMin > min(movAvgDict[key][1]):
 		runningyMin = int(min(movAvgDict[key][1]))
 	if yMax == False and runningyMax < max(movAvgDict[key][1]):
